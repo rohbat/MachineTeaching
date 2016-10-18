4cff5864ee6fd254323138a7685ff29722b4e247
Rohan Batra <rohbat@gmail.com>
2016-10-17 18:16:06 -0700

    Page now updates based on image clicked

--- app.py
+++ app.py
@@ -16,7 +16,11 @@ def get_response():
     if request.method == 'POST':
         data = request.get_data()
         print data
-    return data
+    if data == "0":
+        page_model.main_img, page_model.compare_img_1 = page_model.compare_img_1, page_model.main_img
+    elif data == "1":
+        page_model.main_img, page_model.compare_img_2 = page_model.compare_img_2, page_model.main_img 
+    return jsonify(page_model.get_imgs_list())
 
 @app.route("/")
 def index():
