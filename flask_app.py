d5690d25cbf087374a69f31d34496c84a13dbb52
Nand Kishore <nandck@gmail.com>
2016-10-27 00:46:10 -0700

    This might work.

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify, request, render_template
-#from database import db
-from sqlalchemy import Column, Integer, String
-from flask_sqlalchemy import SQLAlchemy
-#from page_model import PageModel
+from sqlalchemy.orm.session import sessionmaker
+from database import db
+from page_model import PageModel
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -16,44 +15,22 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 
-#db.init_app(app)
-#db.app = app
-db = SQLAlchemy(app)
+db.init_app(app)
+db.app = app
 
-class PageModel(db.Model):
-    __tablename__ = 'triplets'
-    id = Column(Integer, primary_key=True)
-    main_img = Column(String(100))
-    compare_img_1 = Column(String(100))
-    compare_img_2 = Column(String(100))
-    chosen = Column(String(100))
-
-    def __init__(self, main_img, compare_img_1, compare_img_2):
-        self.main_img = main_img
-        self.compare_img_1 = compare_img_1
-        self.compare_img_2 = compare_img_2
-        self.chosen = ''
-
-    def set_chosen(self, img):
-        self.chosen = img
+session = sessionmaker(expire_on_commit=False)
 
 page_model = PageModel("profile1.jpg", "profile2.jpg", "profile3.jpg")
-main_img = page_model.main_img
-compare_img_1 = page_model.compare_img_1
-compare_img_2 = page_model.compare_img_2
-
-def get_imgs_list(page_model):
-    return [main_img, compare_img_1, compare_img_2]
 
 #print 'adding initial pagemodel to session'
-db.session.add(page_model)
+session.add(page_model)
 #print 'commiting initial session'
-db.session.commit()
+session.commit()
 #print 'commited initial session'
 
 @app.route("/get_imgs")
 def get_imgs():
-    return jsonify(get_imgs_list(page_model)) 
+    return jsonify(page_model.get_imgs_list()) 
 
 @app.route("/get_response", methods = ['POST'])
 def get_response():
@@ -66,11 +43,11 @@ def get_response():
         page_model.main_img, page_model.compare_img_2 = page_model.compare_img_2, page_model.main_img 
     
     #print 'adding pagemodel to session on click'
-    db.session.add(page_model)
+    session.add(page_model)
     #print 'commiting pagemodel to session on click'
-    db.session.commit()
+    session.commit()
     #print 'session commited on click'
-    return jsonify(get_imgs_list(page_model))
+    return jsonify(page_model.get_imgs_list())
 
 @app.route("/")
 def index():
