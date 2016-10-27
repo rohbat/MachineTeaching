3b03de5e1b835432866dd5899c5175caf860a70d
Rohan Batra <rohbat@gmail.com>
2016-10-26 23:41:14 -0700

    Added varchar length

--- page_model.py
+++ page_model.py
@@ -4,9 +4,9 @@ from app import db
 class PageModel(db.Model):
     __tablename__ = 'triplets'
     id = Column(Integer, primary_key=True)
-    main_img = Column(String)
-    compare_img_1 = Column(String)
-    compare_img_2 = Column(String)
+    main_img = Column(String(100))
+    compare_img_1 = Column(String(100))
+    compare_img_2 = Column(String(100))
     chosen = Column(String)
 
     def __init__(self, main_img, compare_img_1, compare_img_2):
