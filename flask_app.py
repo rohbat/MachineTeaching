0c9524cd31ba6963551f68780d549ac323c72d6e
Nand Kishore <nandck@gmail.com>
2016-10-27 00:30:31 -0700

    "Fixing errors"

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, render_template
-from database import db
+#from database import db
+from sqlalchemy import Column, Integer, String
 from page_model import PageModel
 
 app = Flask(__name__)
@@ -14,8 +15,29 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 
-db.init_app(app)
-db.app = app
+#db.init_app(app)
+#db.app = app
+db = SQLAlchemy(app)
+
+class PageModel(db.Model):
+    __tablename__ = 'triplets'
+    id = Column(Integer, primary_key=True)
+    main_img = Column(String(100))
+    compare_img_1 = Column(String(100))
+    compare_img_2 = Column(String(100))
+    chosen = Column(String(100))
+
+    def __init__(self, main_img, compare_img_1, compare_img_2):
+        self.main_img = main_img
+        self.compare_img_1 = compare_img_1
+        self.compare_img_2 = compare_img_2
+        self.chosen = ''
+
+    def set_chosen(self, img):
+        self.chosen = img
+
+    def get_imgs_list(self):
+        return [self.main_img, self.compare_img_1, self.compare_img_2]
 
 page_model = PageModel("profile1.jpg", "profile2.jpg", "profile3.jpg")
 
