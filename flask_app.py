Fix bug in set_chosen and gradient order.

flask_app.py
@@ -88,15 +88,15 @@ def get_imgs():
 def get_response():
     if request.method == 'POST':
         data = request.get_data()
+        if data == "0":
+            page_model.set_chosen(page_model.compare_img_1)
+        elif data == "1":
+            page_model.set_chosen(page_model.compare_img_2)
         K = np.zeros((N, N))
         Q = np.zeros((N, N))
         G = np.zeros((N, no_dims))
         tste_grad(user_x_dict[session['name']], N, no_dims, page_model.get_index_list(), 0, no_dims-1, K, Q, G)
         user_x_dict[session['name']] = user_x_dict[session['name']] - (float(eta) / N) * G
-        if data == "0":
-            page_model.set_chosen(page_model.compare_img_1)
-        elif data == "1":
-            page_model.set_chosen(page_model.compare_img_2)
     make_transient(page_model)
     page_model.id = None
     session_sql.add(page_model)