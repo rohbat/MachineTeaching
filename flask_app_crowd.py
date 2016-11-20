52a90a0602fa50932d89f5311c8d160a640c6a5a
whanncar <whanncar@gmail.com>
2016-11-19 19:39:42 -0800

    Worked on flask_app_crowd

--- flask_app_crowd.py
+++ flask_app_crowd.py
@@ -11,6 +11,27 @@ from cython_tste.tste_next_point import *
 
 
 
+
+
+
+# Choose a random triplet and set the page's triplet accordingly
+
+def update_page_with_random():
+    page_ims = random.sample(range(len(image_list)), 3)
+
+    page_model.main_img = page_ims[0]
+    page_model.compare_img_1 = page_ims[1]
+    page_model.compare_img_2 = page_ims[2]
+
+    page_model.main_path = image_list[page_ims[0]]
+    page_model.compare_1_path = image_list[page_ims[1]]
+    page_model.compare_2_path = image_list[page_ims[2]]
+
+
+
+
+
+
 # Make Flask app
 
 app = Flask(__name__)
@@ -40,47 +61,10 @@ session_sql = Session_sql()
 
 
 
-# WHAT AM I DOING?
-
-class_names = glob.glob("/home/cs101teaching/MachineTeaching/static/chinese/ims/*")
-
-class_name_dict = {}
-for class_name in class_names:
-    class_name_dict[class_name] = glob.glob(class_name + "/*")
-
-name_class = {}
-for k, v in class_name_dict.iteritems():
-    for elm in v:
-        name_class[elm] = k
+# Make image list
 
 image_list = glob.glob("/home/cs101teaching/MachineTeaching/static/chinese/ims/*/*")
 image_list.sort()
-
-classes = np.zeros(len(image_list))
-for i in range(len(image_list)):
-    classes[i] = class_names.index(name_class[image_list[i]])
-
-classes_dict = {}
-for i in range(len(class_names)):
-    classes_dict[i] = []
-for i in range(len(classes)):
-    classes_dict[classes[i]].append(i)
-
-print classes, len(classes)
-print classes_dict, len(classes_dict)
-
-
-
-# Set values for tste
-
-N = len(classes)
-no_dims = 10
-alpha = no_dims - 1
-eta = 0.01
-
-
-
-# WHAT AM I DOING?
 
 image_list = [img.replace("/home/cs101teaching/MachineTeaching", "") for img in image_list]
 
@@ -92,28 +76,9 @@ page_model = PageModel()
 
 
 
-# Make dictionaries
-#
-#  - user_x_dict stores the users' kernels
-#  - user_nclicks_dict stores the number of jobs each user has done
+# Make dictionary to keep track of how many jobs each user has done
 
-user_x_dict = {}
 user_nclicks_dict = {}
-
-
-
-# Choose a random triplet and set the page's triplet accordingly
-
-def update_page_with_random():
-    page_ims = random.sample(range(len(image_list)), 3)
-
-    page_model.main_img = page_ims[0]
-    page_model.compare_img_1 = page_ims[1]
-    page_model.compare_img_2 = page_ims[2]
-
-    page_model.main_path = image_list[page_ims[0]]
-    page_model.compare_1_path = image_list[page_ims[1]]
-    page_model.compare_2_path = image_list[page_ims[2]]
 
 
 
@@ -121,6 +86,10 @@ def update_page_with_random():
 
 random.seed()
 update_page_with_random()
+
+
+
+# Redirect user to login page
 
 @app.route("/")
 def to_login():
@@ -140,28 +109,6 @@ def get_imgs():
 #
 # - Gets the response from the user
 # - Updates the user's kernel
-
-@app.route("/teaching/get_response", methods = ['POST'])
-def get_response():
-    if request.method == 'POST':
-        data = request.get_data()
-        if data == "0":
-            page_model.set_chosen(page_model.compare_img_1)
-        elif data == "1":
-            page_model.set_chosen(page_model.compare_img_2)
-        user_nclicks_dict[session['name']] += 1
-        K = np.zeros((N, N))
-        Q = np.zeros((N, N))
-        G = np.zeros((N, no_dims))
-        tste_grad(user_x_dict[session['name']], N, no_dims, page_model.get_index_list(), 0, no_dims-1, K, Q, G)
-        user_x_dict[session['name']] = user_x_dict[session['name']] - (float(eta) / N) * G
-    make_transient(page_model)
-    page_model.id = None
-    session_sql.add(page_model)
-    session_sql.commit()
-
-    update_page_with_random()
-    return jsonify(page_model.get_imgs_list())
 
 @app.route("/kernel/get_response", methods = ['POST'])
 def get_response_kernel():
@@ -186,10 +133,6 @@ def get_response_kernel():
 
 # Render main page
 
-@app.route("/teaching/")
-def index():
-    return render_template('test.html')
-
 @app.route("/kernel/")
 def kernel_index():
     return render_template('kernel.html')
@@ -207,8 +150,7 @@ def login():
     if request.method == 'POST':
         np.save('nclicks.npy', user_nclicks_dict)
         if request.form['username'] != '':
-            session['name'] = request.form['username']
-            user_x_dict[session['name']] = np.random.rand(N, no_dims)
+            session['name'] = request.form['username'] 
             user_nclicks_dict[session['name']] = 0
             return redirect(url_for('kernel_index'))
     return render_template('login.html', error=error)
@@ -219,6 +161,3 @@ def login():
  
 if __name__ == "__main__":
     app.run()
-
-
-
