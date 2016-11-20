31c8a98737b7dfc56669efa8c077c06897b957b5
audhuang <audreynhuang@gmail.com>
2016-11-20 12:48:30 -0800

    creates mturk hit

--- mturk.py
+++ mturk.py
@@ -6,18 +6,19 @@ from boto.mturk.price import Price
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-if os.environ.get("I_AM_IN_DEV_ENV"):
+if os.environ.get("DEV_MODE"):
     HOST = 'mechanicalturk.sandbox.amazonaws.com'
 else:
     HOST = 'mechanicalturk.amazonaws.com'
+    
 
 connection = MTurkConnection(aws_access_key_id=AWS_ACCESS_KEY_ID,
                              aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                              host=HOST)
 
-url = "https://mturk-demonstration.herokuapp.com/"
-title = "Describe a picture in your own words (COMPLETE THIS TASK ONLY ONCE!)"
-description = "COMPLETE THIS TASK ONLY ONCE! All submissions after the first will be rejected"
+url = "https://cs101teaching.pythonanywhere.com/"
+title = "Select the image that is most similar to the target image."
+description = "Select the image that is most similar to the target image."
 keywords = ["easy"]
 frame_height = 800
 amount = 0.05
@@ -25,7 +26,7 @@ amount = 0.05
 questionform = ExternalQuestion(url, frame_height)
 
 
-for _ in xrange(60):
+for _ in xrange(1):
     create_hit_result = connection.create_hit(
         title=title,
         description=description,
