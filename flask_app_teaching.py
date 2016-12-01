61a3a3681547a9df4855565100ff34fafd279127
Rohan Batra <rohbat@gmail.com>
2016-12-01 01:29:42 -0800

    Syntax error oops

--- flask_app_teaching.py
+++ flask_app_teaching.py
@@ -35,7 +35,7 @@ def get_label_list():
     # main_label = str(class_names[classes[page_model_dict[session['name']].main_img]])
     # compare_img_1_label = class_names[classes[page_model_dict[session['name']].compare_img_1]]
     # compare_img_2_label = class_names[classes[page_model_dict[session['name']].compare_img_2]]
-    return (len(classes), str(class_names), str(set(classes))
+    return (len(classes), str(class_names), str(set(classes)))
 
 def get_result_img(result):
     if result == True:
