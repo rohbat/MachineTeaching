3b4c7d4a39cc53c76a34389cff2bc5cc178d7181
Rohan Batra <rohbat@gmail.com>
2016-12-01 00:39:25 -0800

    Debugging

--- flask_app_teaching.py
+++ flask_app_teaching.py
@@ -32,10 +32,10 @@ def update_page_with_random():
 
 
 def get_label_list():
-    main_label = class_names[classes[page_model.main_img]]
-    compare_img_1_label = class_names[classes[page_model.compare_img_1]]
-    compare_img_2_label = class_names[classes[page_model.compare_img_2]]
-    return (main_label, compare_img_1_label, compare_img_2_label)
+    # main_label = class_names[classes[page_model.main_img]]
+    # compare_img_1_label = class_names[classes[page_model.compare_img_1]]
+    # compare_img_2_label = class_names[classes[page_model.compare_img_2]]
+    return ("main_label", "compare_img_1_label", "compare_img_2_label")
 
 def get_result_img(result):
     if result == True:
@@ -207,7 +207,7 @@ def get_response_kernel():
     session_sql.commit()
 
     
-    return jsonify([main_label, c1_label, c2_label, get_result_img(result)])
+    return jsonify(["main_label", "c1_label", "c2_label", "get_result_img(result)"])
 
 
 
