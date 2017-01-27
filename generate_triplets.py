905433c807f9660629382b81fcd5f5537acb6d6f
audhuang <audreynhuang@gmail.com>
2017-01-27 10:26:01 -0800

    trying to visualize kernel after

--- generate_triplets.py
+++ generate_triplets.py
@@ -5,23 +5,25 @@ from cython_tste import cy_tste
 import cPickle
 import random
 
-random.seed(1)
-numpy.random.seed(1)
-#db_data = PageModel.query.all()
-f = open("triplets_chinese_chars.txt", "rb")
-db_data = cPickle.load(f)
-f.close()
-print len(db_data)
-triplets = []
-for pg_model in db_data:
-    indices = pg_model.get_index_list()
-    if not None in indices:
-        triplets.append(indices)
+# random.seed(1)
+# numpy.random.seed(1)
+# #db_data = PageModel.query.all()
+# f = open("triplets_chinese_chars.txt", "rb")
+# db_data = cPickle.load(f)
+# f.close()
+# print len(db_data)
+# triplets = []
+# for pg_model in db_data:
+#     indices = pg_model.get_index_list()
+#     if not None in indices:
+#         triplets.append(indices)
         
-triplets = numpy.array(triplets)
-print triplets
+# triplets = numpy.array(triplets)
+# print triplets
+triplets = np.load('user_x_dict_seabed.npy')
+print(triplets.shape)
 embedding = cy_tste.tste(triplets,
-     no_dims=10,
+     no_dims=5,
      lamb=0,
      alpha=None,
      verbose=True,
@@ -34,6 +36,6 @@ embedding = cy_tste.tste(triplets,
      use_log=False,
      )
 print embedding
-numpy.save("X_initial_chinese_10", embedding)
-embedding1 = numpy.load("X_initial_chinese_10.npy")
+numpy.save("X_after_seabed", embedding)
+embedding1 = numpy.load("X_after_seabed.npy")
 print embedding1
