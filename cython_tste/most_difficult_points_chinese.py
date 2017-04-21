e014285015018fbeac4561be24c0e2f685b0f37c
Rohan Batra <rohbat@gmail.com>
2017-04-20 22:14:15 -0700

    Added/modified code to get 100 hardest triplets and 100 triplets with p closest to .5

--- cython_tste/most_difficult_points_chinese.py
+++ cython_tste/most_difficult_points_chinese.py
@@ -4,6 +4,8 @@ import operator
 import glob
 import os
 
+LOCAL = True
+
 def compute_kernel_at_pair(X, N, pair, no_dims, alpha):
     i, j = pair
     diff = X[i, :] - X[j, :]
@@ -13,7 +15,8 @@ def compute_kernel_at_pair(X, N, pair, no_dims, alpha):
 
 def most_uncertain_triplets(train, X,N,no_dims,alpha,classes,classes_dict):
 
-    triplet_probs = {}
+    triplet_probs5 = {}
+    triplet_probs0 = {}
 
     for i in train:
         print i
@@ -24,24 +27,34 @@ def most_uncertain_triplets(train, X,N,no_dims,alpha,classes,classes_dict):
             for k in classes_dict['not'+str(classes[i])]:
                 k_ik = compute_kernel_at_pair(X, N, (i, k), no_dims, alpha)
                 P = k_ij / (k_ij + k_ik)
-                triplet_probs[(i, j, k)] = abs(P - 0.5)
+                if (abs(P - 0.5) < .01):
+                    triplet_probs5[(i, j, k)] = abs(P - 0.5)
+                if (P < .01):
+                    triplet_probs0[(i, j, k)] = abs(P)
 
-    sorted_triplets = sorted(triplet_probs.items(), key=operator.itemgetter(1))
+    sorted_triplets5 = sorted(triplet_probs5.items(), key=operator.itemgetter(1))
+    sorted_triplets0 = sorted(triplet_probs0.items(), key=operator.itemgetter(1))
 
-    np.save('sorted_triplets.npy', sorted_triplets)
-    hardest100 = sorted_triplets[:100]
-    hard4 = random.sample(hardest100, 4)
+    np.save('sorted_triplets5.npy', sorted_triplets5)
+    np.save('sorted_triplets0.npy', sorted_triplets0)
+    hardest100_5 = sorted_triplets5[:100]
+    hardest100_0 = sorted_triplets0[:100]
 
-    return hardest100, hard4
+    return hardest100_5, hardest100_0
 
 def main():
+    if (LOCAL == True):
+        sub_path = "/MachineTeaching/static/machine_teaching_data/chinese/ims/"
+    else:
+        sub_path = "/MachineTeaching/static/chinese/ims/"
+
     X = np.load("MachineTeaching/static/X_initial_chinese.npy")
     no_dims = 5
     alpha = no_dims - 1.0
 
     # Set up classes and classes_dict_chinese
     path = os.getcwd()
-    class_names = glob.glob(path + "/MachineTeaching/static/chinese/ims/*")
+    class_names = glob.glob(path + sub_path + "*")
     class_names.sort()
 
     class_name_dict_chinese = {}
@@ -53,7 +66,7 @@ def main():
         for elm in v:
             name_class[elm] = k
 
-    image_list = glob.glob(path + "/MachineTeaching/static/chinese/ims/*/*")
+    image_list = glob.glob(path + sub_path + "*/*")
     image_list.sort()
     N = len(image_list)
     print N
@@ -64,7 +77,7 @@ def main():
         classes[i] = class_names.index(name_class[image_list[i]])
 
 
-    class_names = [c.replace(path + "/MachineTeaching/static/chinese/ims/", "") for c in class_names]
+    class_names = [c.replace(path + sub_path, "") for c in class_names]
     print 'class names: ', class_names
 
     classes_dict_chinese = {}
@@ -81,9 +94,9 @@ def main():
                 classes_dict_chinese['not'+str(key)].extend(classes_dict_chinese[key1])
 
     print classes_dict_chinese
-    hardest100, hard4 = most_uncertain_triplets(train, X,N,no_dims,alpha,classes,classes_dict_chinese)
-    print hardest100
-    print hard4
+    hardest100_5, hardest100_0 = most_uncertain_triplets(train, X,N,no_dims,alpha,classes,classes_dict_chinese)
+    print hardest100_5
+    print hardest100_0
 
 
 if __name__ == '__main__':
