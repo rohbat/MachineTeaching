7d277833981f8076ed901eb058b9f3354bafe7ae
audhuang <audreynhuang@gmail.com>
2017-06-28 08:08:18 -0700

    initial sim results

---
+++ cython_tste/sim_analysis.py
@@ -0,0 +1,141 @@
+from __future__ import division, print_function
+import numpy as np
+import random 
+import matplotlib.pyplot as plt
+import os
+import glob
+from tste_next_point import *
+from simulation import *
+
+img_path = '\\Users\\Audrey\\Documents\\birds\\images\\'
+def get_classes(path): 
+    class_names = glob.glob(path + '*')
+    class_names.sort()
+
+    class_name_dict_bird = {}
+    for class_name in class_names:
+        class_name_dict_bird[class_name] = glob.glob(class_name + "\\*")
+
+    name_class = {}
+    for k, v in class_name_dict_bird.items():
+        for elm in v:
+            name_class[elm] = k
+
+    image_list = glob.glob(path + "*\\*")
+    image_list.sort()
+    N = len(image_list)
+
+    classes = np.zeros(len(image_list), dtype=int)
+    for i in range(len(image_list)):
+        classes[i] = class_names.index(name_class[image_list[i]])
+
+
+    class_names = [c.replace(path, "") for c in class_names]
+
+    classes_dict_bird = {}
+    for i in range(len(class_names)):
+        classes_dict_bird[i] = []
+
+    for i in range(N):
+        classes_dict_bird[classes[i]].append(i)
+
+    classes_dict_copy = classes_dict_bird.copy()
+
+    for key in classes_dict_bird.keys():
+        classes_dict_copy['not'+str(key)] = []
+        for key1 in classes_dict_bird.keys():
+            if key != key1 and not 'not' in str(key1):
+                classes_dict_copy['not'+str(key)].extend(classes_dict_bird[key1])
+
+    return classes, classes_dict_copy
+
+
+
+    # return classes, classes_dict
+
+classes, classes_dict = get_classes(img_path)
+
+
+# def show_triplets(path): 
+# 	triplet_dict = np.load(path).item()
+
+# 	for key in range(4): 
+# 		images = triplet_dict[key]
+# 		for i in range(len(images)): 
+# 			imgs = images[i]
+# 			print(imgs)
+# 			# for j in imgs: 
+# 			# 	img = Image.open(img_path + imgs[i])
+#    #      img.resize((200, 200), Image.ANTIALIAS) # resizes image in-place
+#    #      plt.imshow(img)
+#    #      plt.axis('off')
+#    #  plt.savefig(path + '/MachineTeaching/imgs/' + str(user_selection_method_dict_bird[session['name']]) + '_' + \
+#    #      str(user_nclicks_dict_bird[session['name']]))
+
+
+n_teach = 5
+n_test = 500 
+rounds = 6
+user = 'perf'
+train = range(N)
+labels = ['rand', 'uncertain', 'BG', 'BG Rand']
+
+X = np.load('\\Users\\Audrey\\Documents\\cs101a\\static\\X_initial_bird.npy')
+def test_0(n, X, classes, classes_dict_bird): 
+	acc = 0
+	for i in range(n): 
+		(main, comp1, comp2) = random_triplet(train, classes, classes_dict_bird)
+		chosen = teach_response_maj(main, comp1, comp2, X)
+
+		if classes[chosen[0]] == classes[chosen[1]]: 
+			acc += 1
+	return acc / n
+acc_0 = test_0(n_test, X, classes, classes_dict)
+print(acc_0)
+
+def test_acc(ans): 
+	acc = np.zeros([rounds])
+
+	for i in range(rounds): 
+		for j in range(n_test):
+			index = n_test * i + j
+			triplet = ans[index].astype(int)
+			# print(classes[triplet[0]], classes[triplet[1]], classes[triplet[2]])
+
+			if classes[triplet[0]] == classes[triplet[1]]: 
+				acc[i] += 1
+
+	return acc 
+
+
+
+
+def graph_testing(path): 
+	test_dict = np.load(path + 'test_dict_' + user + '.npy').item()
+	print(test_dict.keys())
+	print(np.shape(test_dict[0]))
+
+	plt.figure()
+	for i in range(4): 
+		ans = np.mean(test_dict[i], axis=0)
+		acc = test_acc(ans) / n_test
+		print(acc)
+		
+		plt.plot(range(rounds + 1), [acc_0] + list(acc))
+		plt.xlabel('Learning Iteration (% 5)')
+		plt.ylabel('Accuracy')
+		plt.title('Test Accuracy over 30 Teaching Examples, 30 Users')
+	plt.legend(labels, loc = 'upper left')
+	plt.savefig('./test_' + str(user))
+
+
+def main(): 
+	path = '..\\sim\\'
+	graph_testing(path)
+	
+	# visualize(triplet_dict, acc_dict, X_dict, X)
+
+
+
+if __name__ == '__main__':
+	main()
--- cython_tste/simulation.py
+++ cython_tste/simulation.py
@@ -63,7 +63,8 @@ alpha = no_dims - 1.0
 eta = 0.1
 lamb = 0
 train = range(N)
-user = 'perfect'
+user = 'perf_2'
+num_user = 30
 
 
 
@@ -93,31 +94,59 @@ def teach_response_perf(i, j, k, X):
         return [i, k, j]
 
 
+def teach_response_rand(i, j, k, x): 
+    r = np.random.uniform(0., 1.)
+    if r < 0.5: 
+        return [i, j, k]
+    else: 
+        return [i, k, j]
+
+def teach_response_maj(i, j, k, X): 
+    diff = X[i, :] - X[j, :]
+    k_ij = np.dot(diff, diff)
+    k_ij = (1 + k_ij / alpha) ** ((alpha + 1) / -2)
+    
+    diff = X[i, :] - X[k, :]
+    k_ik = np.dot(diff, diff)
+    k_ik = (1 + k_ik / alpha) ** ((alpha + 1) / -2)
+    
+    p =  k_ij / (k_ij + k_ik)
+
+    if p >= 0.5: 
+        return [i, j, k]
+    else: 
+        return [i, k, j]
+
+
+
+
 
 def teach(n, rounds, triplet_dict, X_dict, classes, classes_dict_bird): 
     print('round: ', rounds)
     for i in range(n): 
         for key in range(4): 
-            X = X_dict[key]
-            K = np.zeros((N, N))
-            Q = np.zeros((N, N))
-            G = np.zeros((N, no_dims))
-            
-            if key == 0: 
-                (main, comp1, comp2) = random_triplet(train, classes, classes_dict_bird)
-            elif key == 1: 
-                ((main, comp1, comp2), p) = most_uncertain_triplet(train,X,N,no_dims,alpha,lamb,classes,classes_dict_bird,eta,no_classes=no_classes,sample_class = sample_class)
-            elif key == 2: 
-                ((main, comp1, comp2), p) = best_gradient_triplet(train,X,N,no_dims,alpha,lamb,classes,classes_dict_bird,eta,no_classes=no_classes,sample_class = sample_class)
-            elif key == 3: 
-                ((main, comp1, comp2), p) = best_gradient_triplet_rand_evaluation(train,X,N,no_dims,alpha,lamb,classes,classes_dict_bird,eta,no_classes=no_classes,sample_class = sample_class)
-            
+            for j in range(num_user): 
+                X = X_dict[key][j]
+                K = np.zeros((N, N))
+                Q = np.zeros((N, N))
+                G = np.zeros((N, no_dims))
+                
+                if key == 0: 
+                    (main, comp1, comp2) = random_triplet(train, classes, classes_dict_bird)
+                elif key == 1: 
+                    ((main, comp1, comp2), p) = most_uncertain_triplet(train,X,N,no_dims,alpha,lamb,classes,classes_dict_bird,eta,no_classes=no_classes,sample_class = sample_class)
+                elif key == 2: 
+                    ((main, comp1, comp2), p) = best_gradient_triplet(train,X,N,no_dims,alpha,lamb,classes,classes_dict_bird,eta,no_classes=no_classes,sample_class = sample_class)
+                elif key == 3: 
+                    ((main, comp1, comp2), p) = best_gradient_triplet_rand_evaluation(train,X,N,no_dims,alpha,lamb,classes,classes_dict_bird,eta,no_classes=no_classes,sample_class = sample_class)
+                      
+                chosen = teach_response_perf(main, comp1, comp2, X)
+                tste_grad(X, N, no_dims, chosen, 0, no_dims-1.0, K, Q, G)
+                X_dict[key][j] = X - 0.4 * G
+                # print(key, np.sum(G))
+                triplet_dict[key][j, n*rounds + i] = np.asarray(chosen)
             print('triplet: ', main, comp1, comp2, i, key)
-            chosen = teach_response_perf(main, comp1, comp2, X)
-            tste_grad(X, N, no_dims, chosen, 0, no_dims-1.0, K, Q, G)
-            X_dict[key] = X - 0.4 * G
-
-            triplet_dict[key][n*rounds + i] = np.asarray(chosen)
+    
     return triplet_dict, X_dict
 
 
@@ -125,11 +154,12 @@ def teach(n, rounds, triplet_dict, X_dict, classes, classes_dict_bird):
 def test(n, rounds, acc_dict, X_dict, classes, classes_dict_bird): 
     for i in range(n): 
         for key in range(4): 
-            X = X_dict[key]
-            (main, comp1, comp2) = random_triplet(train, classes, classes_dict_bird)
-            chosen = teach_response_perf(main, comp1, comp2, X)
+            for j in range(num_user): 
+                X = X_dict[key][j]
+                (main, comp1, comp2) = random_triplet(train, classes, classes_dict_bird)
+                chosen = teach_response_maj(main, comp1, comp2, X)
 
-            acc_dict[key][n*rounds + i] = np.asarray(chosen)
+                acc_dict[key][j, n*rounds + i] = np.asarray(chosen)
 
             # acc_dict[key][n*rounds + i] = ans
     return acc_dict
@@ -150,15 +180,18 @@ def main():
 
     triplet_dict = {}
     test_dict = {}
-    X_dict = {}
+    # X_dict = {}
     n_teach = 5
-    n_test = 100 
+    n_test = 500 
     rounds = 6
 
     for i in range(4): 
-        triplet_dict[i] = np.zeros([n_teach * rounds, 3])
-        test_dict[i] = np.zeros([n_test * rounds, 3])
-        X_dict[i] = X
+        triplet_dict[i] = np.zeros([num_user, n_teach * rounds, 3])
+        test_dict[i] = np.zeros([num_user, n_test * rounds, 3])
+        # X_dict[i] = np.zeros([num_user, np.shape(X)[0], np.shape(X)[1]])
+        # for j in range(num_user): 
+        #     X_dict[i][j] = X
+    X_dict = np.load('\\Users\\Audrey\\Documents\\cs101a\\sim\\X_dict_perf_5.npy').item()
 
     for i in range(rounds): 
         triplet_dict, X_dict = teach(n_teach, i, triplet_dict, X_dict, classes, classes_dict)
